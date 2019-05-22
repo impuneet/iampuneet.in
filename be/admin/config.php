4eb4e79db3da28ad1aa95e3d1bf82d037b8f3157
Puneet Sharma <iampuneet.in@gmail.com>
2019-05-22 15:21:14 +0530

    saving intial

---
+++ be/admin/config.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+/* URL PROJECT */
+
+define ('SITE_URL', 'http://iampuneet.in/be');
+
+/* DATABASE CONFIGURATION */
+
+$database = array(
+'host' => 'mi3-ss13.a2hosting.com',
+'db' => 'maharaja_mobapp',
+'user' => 'maharaja_mobApp',
+'pass' => '9569595695'
+);
+
+$email_config = array(
+'email_address' => 'EMAIL_ADDRESS_HERE',
+'email_password' => 'PASSWORD_HERE',
+'email_subject' => 'EMAIL_SUBJECT_HERE',
+'email_name' => 'EMAIL_NAME_HERE',
+'smtp_host' => 'EMAIL_HOST_HERE',
+'smtp_port' => 'EMAIL_PORT_HERE',
+'smtp_encrypt' => 'tls'
+);
+
+$items_config = array(
+    
+    'items_per_page' => '8',
+    'images_folder' => 'images/'
+);
+
+
+?>
